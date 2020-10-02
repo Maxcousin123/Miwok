fefe292902a8f666bd2edbf7e83611cf211a3b9b
Maxcousin123 <66350396+Maxcousin123@users.noreply.github.com>
2020-10-02 06:09:06 +0200

    Hello

--- app/src/main/java/android/example/miwok/WordAdapter.java
+++ app/src/main/java/android/example/miwok/WordAdapter.java
@@ -1,22 +1,47 @@
 package android.example.miwok;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+
 public class WordAdapter extends ArrayAdapter<Word>  {
 
-    public WordAdapter(@NonNull Context context, int resource) {
-        super(context, resource);
+    public WordAdapter(Context context, ArrayList<Word> words) {
+        super(context, 0, words);
     }
 
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+    public View getView(int position,View  convertView, ViewGroup parent) {
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        Word currentWord = getItem(position);
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        miwokTextView.setText(currentWord.getMiwokTranslation());
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+        return listItemView;
+
+
+
+
+
+
+
+
+
+
     }
 }
